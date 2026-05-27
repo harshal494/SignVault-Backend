81c4ae2b5b2ad6708dac9a8d02357f2f063b5f97
harshal494 <harshalkhade494@gmail.com>
2026-05-27 17:24:21 +0530

    sprint 3 complete - auth APIs working, tested in Postman

--- src/main/java/com/harshalkhade/signvault/repository/OtpVerificationRepository.java
+++ src/main/java/com/harshalkhade/signvault/repository/OtpVerificationRepository.java
@@ -5,11 +5,14 @@ import com.harshalkhade.signvault.entity.User;
 import com.harshalkhade.signvault.enums.OtpType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface OtpVerificationRepository extends JpaRepository<OtpVerification, Long> {
     Optional<OtpVerification> findByUserAndTypeAndVerified(User user, OtpType type, boolean verified);
+
+    @Transactional
     void deleteByUser(User user);
 }
