1858343ad41d7786d3620a1e87c98b3fe05e5970
harshal494 <harshalkhade494@gmail.com>
2026-07-15 18:55:05 +0530

    Disable global JVM network proxy overriding paths

--- src/main/java/com/harshalkhade/signvault/SignvaultApplication.java
+++ src/main/java/com/harshalkhade/signvault/SignvaultApplication.java
@@ -11,6 +11,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class SignvaultApplication {
 
 	public static void main(String[] args) {
+
+		System.clearProperty("http.proxyHost");
+		System.clearProperty("http.proxyPort");
+		System.clearProperty("https.proxyHost");
+		System.clearProperty("https.proxyPort");
+		System.setProperty("java.net.useSystemProxies", "false");
+		
 		SpringApplication.run(SignvaultApplication.class, args);
 	}
 
